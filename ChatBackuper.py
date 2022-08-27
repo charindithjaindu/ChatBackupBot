9b6d9e33e6b742dc9264a2d0a592892ada5daf57
Jaindu Charindith <75799963+charindithjaindu@users.noreply.github.com>
2022-08-27 09:42:44 +0530

    Update ChatBackuper.py

--- ChatBackuper.py
+++ ChatBackuper.py
@@ -14,16 +14,12 @@ log_channel=os.getenv("LOG_CHANNEL")
 myuserid=os.getenv("OWNER_UNAME")
 
 BOT_url='https://api.telegram.org/bot'+TOKEN
-app=Client(session, api_id, api_hash)
+app=Client('CharindithBAckupBot', api_id,api_hash,session_string=session)
 
-
-def utc_to_time(naive, timezone="Asia/Kolkata"):
-    dt_object = datetime.fromtimestamp(naive)
-    return dt_object.replace(tzinfo=pytz.utc).astimezone(pytz.timezone(timezone))
 
 async def dirup(message,pat,tgapi,otherr):
 	pat=pat[:-1]
-	tes =tgapi+'?caption='+otherr+ str(message.chat.username)+" "+str(message.chat.first_name) +"\n"+str(message.caption)+"\n"+str(utc_to_time(message.date))
+	tes =tgapi+'?caption='+otherr+ str(message.chat.username)+" "+str(message.chat.first_name) +"\n"+str(message.caption)+"\n"+str((message.date))
 	arr = os.listdir(pat)
 	for files in arr:
 		pathh=pat+"/"+str(files)
@@ -38,20 +34,17 @@ async def dirup(message,pat,tgapi,otherr):
 def botSend(fileName, tes ,pat):
     files = {pat: (fileName, open(fileName,'rb'))}  
     r = requests.post(BOT_url+tes+'&chat_id='+str(log_channel), files=files)
-    print(r)
-
 
 
 @app.on_message(filters.text & filters.private & ~filters.bot)
 async def msg_text(client: Client, message: Message):
 	print('text recived')
 	if message.from_user.username == myuserid:
-		tes ="From @"+myuserid+" to @"+ str(message.chat.username)+" "+str(message.chat.first_name) +"\n"+str(message.text)+"\n"+str(utc_to_time(message.date))
+		tes ="From @"+myuserid+" to @"+ str(message.chat.username)+" "+str(message.chat.first_name) +"\n"+str(message.text)+"\n"+str((message.date))
 	else:
-		tes ="@"+ str(message.from_user.username)+" "+str(message.chat.first_name) +"\n"+str(message.text)+"\n"+str(utc_to_time(message.date))
+		tes ="@"+ str(message.from_user.username)+" "+str(message.chat.first_name) +"\n"+str(message.text)+"\n"+str((message.date))
 	g=requests.post(BOT_url+'/sendmessage' , json={"chat_id":log_channel,"text":tes})
 	print(tes)
-	print(g)
 
 @app.on_message(filters.photo & filters.private & ~filters.bot)
 async def msg_photo(client: Client, message: Message):
